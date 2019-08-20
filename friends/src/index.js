959c10d3a0c43ffb3475c10ab6223afe9ec3a3ce
ElijahMcKay <45011056+ElijahMcKay@users.noreply.github.com>
2019-08-20 13:18:51 -0500

    react router installed and setup

--- friends/src/index.js
+++ friends/src/index.js
@@ -5,9 +5,9 @@ import App from './App';
 import BrowserRouter from 'react-router-dom'; 
 
 ReactDOM.render(
-<BrowserRouter>
+<BrowserRouter>  {/*setting up react router*/}
     <App />
-</BrowserRouter>, document.getElementById('root'));
+</BrowserRouter>, document.getElementById('root')); 
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

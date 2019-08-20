3bb8fd9d36cd26bd5680aa45b89024d0c2cb3f90
ElijahMcKay <45011056+ElijahMcKay@users.noreply.github.com>
2019-08-20 16:03:22 -0500

    setting up react router

---
+++ friends/src/components/Friends.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+function Friends() {
+
+    return (
+        <>
+
+            <p>YOURE NOT SUPPOSED TO SEE THIS</p>
+        </>
+    )
+}
+
+export default Friends; 
